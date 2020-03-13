c91fc76c3baaea6a01e9321220b1d2ce3ac6cafc
daniao2017 <2868108923@qq.com>
2020-03-13 10:28:25 +0800

    777

---
+++ 数据结构/第二章/数组移位.c
@@ -0,0 +1,26 @@
+#include<stdio.h>;
+void Shift(int  Array[],int N);
+#define MAXN 100
+int main(){
+    int Number[MAXN],N,M;
+    int i;
+    scanf("%d%d",&N,&M);
+    for(i=0;i<N;i++)
+        scanf("%d",&Number[i]);
+    M %=N; /*将m大于等于n时转化为等价的小于n的数*/
+    for(i=0;i<M;i++)
+        Shift(Number,N);/*n个元素循环一位*/
+    for(i=0;i<N-1;i++) /*打印输出*/
+        printf("%d",Number[i]);
+    printf("%d\n",Number[i]);
+    return 0;
+}
+
+void Shift(int  Array[],int N)
+{
+    int i,ArrayEnd;
+    ArrayEnd = Array[N-1];
+    for (i=N-1;i>0;i--) /*n个元素循环一位*/
+        Array[i]=Array[i-1];
+    Array[0]=ArrayEnd;
+}
